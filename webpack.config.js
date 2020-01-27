f76a904a889a158faf589ad46d302a87dc3f8448
Moe Shaaban <info@mshaaban.com>
2020-01-27 14:32:39 +0100

    add webpack to check if it works

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './index.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.webpack.js',
     path: path.resolve(__dirname, 'dist'),
   },
   mode: 'production',
